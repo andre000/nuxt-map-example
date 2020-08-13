1a4dc0b6e5676a8441673e5c0a5de019c4f8f3da
André Adriano <andre@dev-next.com>
2020-08-13 17:46:04 -0300

    :sparkles: Add page and pageSize variables to store

--- store/index.js
+++ store/index.js
@@ -1,10 +1,12 @@
 import roomData from '../data/residencias.json'
 import averagePriceData from '../data/media_preco.json'
 
-export const store = {
+export const store = () => ({
+  page: 1,
+  pageSize: 5,
   rooms: [],
   averagePrice: []
-}
+})
 
 export const mutations = {
   SET_ROOMS (state, rooms) {
@@ -13,13 +15,22 @@ export const mutations = {
 
   SET_AVERAGE_PRICE (state, averagePrice) {
     state.averagePrice = averagePrice
+  },
+
+  SET_PAGE (state, page) {
+    state.page = page
+  },
+  SET_PAGE_SIZE (state, size) {
+    state.pageSize = size
   }
 }
 
 export const actions = {
-  nuxtServerInit ({ dispatch }) {
+  nuxtServerInit ({ dispatch, commit }) {
     dispatch('loadRooms')
     dispatch('loadAveragePrice')
+    commit('SET_PAGE', 1)
+    commit('SET_PAGE_SIZE', 5)
   },
 
   loadRooms ({ commit }) {
@@ -27,5 +38,11 @@ export const actions = {
   },
   loadAveragePrice ({ commit }) {
     commit('SET_AVERAGE_PRICE', averagePriceData)
+  }
+}
+
+export const getters = {
+  paginatedRooms (state) {
+    return state.rooms.slice(0, state.page * state.pageSize)
   }
 }
