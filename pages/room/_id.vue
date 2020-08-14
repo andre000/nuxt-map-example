d8a091a713f17dccb6ef7561b487139650792f3d
André <andre@dev-next.com>
2020-08-14 00:00:14 -0300

    🚧 Create dynamic route to rooms

---
+++ pages/room/_id.vue
@@ -0,0 +1,29 @@
+<template>
+  <div v-if="selectedRoom">
+    <h1>{{ selectedRoom.name }}</h1>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+  data: () => ({
+    selectedRoom: null
+  }),
+
+  computed: {
+    ...mapState(['rooms'])
+  },
+
+  created () {
+    this.selectedRoom = this.rooms.find(r => Number(r.id) === Number(this.$route.params.id))
+    if (!this.selectedRoom) { return this.$router.push('/') }
+  }
+
+}
+</script>
+
+<style>
+
+</style>
